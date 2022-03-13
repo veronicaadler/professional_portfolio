c5a9084c699e7a34a30fb0d455835ca4dde64849
Veronica Adler <vlynnadler@gmail.com>
2022-03-13 19:20:20 -0400

    set up conditional icon rendering for right and down arrows

--- src/components/home/HomeComponent.js
+++ src/components/home/HomeComponent.js
@@ -1,7 +1,20 @@
 import { Button, Container, Row, Col} from 'react-bootstrap'
 import FlyingLottie from './FlyingLottieComponent'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
 
 const HomeComponent = () => {
+
+
+    const [iconChoice, setIconChoice ] = useState(true)
+    console.log(iconChoice)
+
+    const handleMouse = () => {
+        setIconChoice(!iconChoice)
+    }
+
 
     return (
         <Container className="home text-center">
@@ -15,8 +28,11 @@ const HomeComponent = () => {
             <Row>
              <Col>
             <h1 className="display-2">Hi! I'm <span className="bizname">Veronica Adler</span>,</h1>
-            <h2 className="pb-0"> a front end web developer.</h2>
-            <Button className="homebtn btn-light">See My Projects</Button>
+            <h2 className="pb-0"> a front end web developer. </h2>
+            <Button role="button" onMouseEnter={handleMouse} onMouseLeave={handleMouse} className="homebtn btn-light"><span>See My Projects </span>  
+                {iconChoice && <FontAwesomeIcon icon={faArrowRight} />}
+                {!iconChoice && <FontAwesomeIcon icon={faArrowDown} />}
+            </Button>
             </Col>
             </Row>
             </Row>
