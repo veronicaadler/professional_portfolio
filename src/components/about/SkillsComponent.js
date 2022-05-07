dd794bff515058691631f65baa62adc80f35f529
Veronica Adler <vlynnadler@gmail.com>
2022-05-06 23:47:49 -0400

    created objects for storing skill cards and initial set up of skill page array.

--- src/components/about/SkillsComponent.js
+++ src/components/about/SkillsComponent.js
@@ -1,7 +1,23 @@
+import SkillCards from "../../shared/SkillCardData";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup'
+
 const SkillsComponent = () => {
     return (
         <div id="skills">
             <h1>Skills</h1>
+            <CardGroup>
+            {SkillCards.map((skill) => {
+                return (
+                <Card>
+                <Card.Img variant="top" src={skill.img} />
+                <Card.Body>
+                  <Card.Title>{skill.title}</Card.Title>
+                </Card.Body>
+              </Card>
+                )
+            })}
+            </CardGroup>
         </div>
       );
 }
--- src/shared/SkillCardData.js
+++ src/shared/SkillCardData.js
@@ -1,7 +1,55 @@
 const SkillCards = [
     {
+        id: 0,
+        title: "Javascript",
+        img: "assets/JS.jpeg"
+    },
+    {
+        id: 1,
+        title: "React.js",
+        img: "assets/React.png"
+    },
+    {
+        id: 2,
+        title: "HTML",
+        img: "assets/HTML5.png"
+    },
+    {
+        id: 3,
+        title: "CSS",
+        img: "assets/CSS.png"
+    },
+    {
+        id: 4,
+        title: "Bootstrap",
+        img: "assets/Bootstrap.png"
+    },
+    {
+        id: 5,
+        title: "Next.js",
+        img: "assets/Next.png"
+    },
+    {
+        id: 6,
+        title: "React Native",
+        img: "assets/ReactNative.png"
+    },
+    {
+        id: 7,
+        title: "React Testing Library",
+        img: "assets/ReactTestingLibrary.png"
+    },
+    {
+        id: 8,
+        title: "Git",
+        img: "assets/Git.png"
+    },
+    {
+        id: 9,
+        title: "Github",
+        img: "assets/Github.png"
+    },
 
-    }
 
 ]
 
